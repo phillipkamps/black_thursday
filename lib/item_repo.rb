b351874c4d4aef528a0ba09ca66d5098cbebfa2d
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-28 11:32:37 -0700

    Feat: create Customer class, Test: add prelim Customer tests

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -1,7 +1,5 @@
-
 require "pry"
 class ItemRepository
-
   # attributes ...
   attr_reader :items_instances_array
 
@@ -15,40 +13,40 @@ class ItemRepository
 
   def find_by_id(id)
     items_instances_array.find do |item_instance|
-    item_instance.item_attributes[:id] == id
+      item_instance.item_attributes[:id] == id
     end
   end
 
   def find_by_name(name)
     items_instances_array.find do |item_instance|
-    item_instance.item_attributes[:name] == name.downcase
+      item_instance.item_attributes[:name] == name.downcase
     end
   end
 
   def find_all_with_description(string)
     items_instances_array.find_all do |item_instance|
       item_instance.item_attributes[:description].include?(string)
-      end
+    end
   end
 
   def find_all_by_price(integer)
     items_instances_array.find_all do |item_instance|
-    item_instance.item_attributes[:unit_price] == integer.to_s
+      item_instance.item_attributes[:unit_price] == integer.to_s
     end
   end
 
   def find_all_by_price_in_range(range)
     range_items_to_check = []
     items_instances_array.each do |item_instance|
-        range_items_to_check << (item_instance.item_attributes[:unit_price].to_f <= range.to_a.max && item_instance.item_attributes[:unit_price].to_f >= range.to_a.min)
+      range_items_to_check << (item_instance.item_attributes[:unit_price].to_f <= range.to_a.max && item_instance.item_attributes[:unit_price].to_f >= range.to_a.min)
     end
-    return range_items_to_check
+    range_items_to_check
   end
 
   def find_all_by_merchant_id(input_id)
     items_instances_array.find_all do |item_instance|
       item_instance.item_attributes[:merchant_id].equal?(input_id)
-      end
+    end
   end
 
   def create(attributes)
@@ -64,17 +62,16 @@ class ItemRepository
   def update(id, attributes)
     if attributes.include?(:name)
       find_by_id(id).item_attributes[:name] = attributes[:name]
-      end
+    end
     if attributes.include?(:description)
       find_by_id(id).item_attributes[:description] = attributes[:description]
-      end
+    end
     if attributes.include?(:unit_price)
       find_by_id(id).item_attributes[:unit_price] = attributes[:unit_price]
-      end
+    end
   end
 
   def delete(id)
     items_instances_array.delete(find_by_id(id))
   end
-
 end
