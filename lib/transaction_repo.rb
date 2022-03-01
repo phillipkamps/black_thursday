1a700594da074d96793a34c51c6b735a12d2bbab
phdd0 <paul.haddick@outlook.com>
2022-02-28 22:43:55 -0700

    feat: build repo search & crud mtd shells to start trans repo tests

--- lib/transaction_repo.rb
+++ lib/transaction_repo.rb
@@ -11,4 +11,34 @@ class TransactionRepository
     transactions_instances_array
   end
 
+  def find_by_id
+    # nil OR instance of Transaction with matching ID
+  end
+
+  def find_all_by_invoice_id
+    # [] OR >= 1 matches
+  end
+
+  def find_all_by_credit_card_number
+    # [] OR >= 1 matches
+  end
+
+  def find_all_by_result
+    # [] OR >=1 matches
+  end
+
+  def create(attributes)
+    # new Transaction instance w/attrib provided; curr HIGHEST TrID += 1
+  end
+
+  def update(id, attributes)
+    # update Transaction instance having given id w/attrib provided;
+    # ONLY update: CC-number, exp-date, result;
+    # changes updated_at to be Time.now
+  end
+
+  def delete(id)
+    # Transaction instance with given id
+  end
+
 end
